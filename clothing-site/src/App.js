b917a6f6ef601d80e8c56dadc1ba4e6fb0f51ba9
unknown <joshbrogdon5@gmail.com>
2019-01-04 14:42:44 -0700

    setting up home component/route

--- clothing-site/src/App.js
+++ clothing-site/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Clothing Store
+        {routes}
       </div>
     );
   }
