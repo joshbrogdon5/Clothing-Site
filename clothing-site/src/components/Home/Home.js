316e71ca5bb6049cff1acbb57c73d5f8b20907d1
unknown <joshbrogdon5@gmail.com>
2019-01-04 14:58:28 -0700

    setup nav skeleton

--- clothing-site/src/components/Home/Home.js
+++ clothing-site/src/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Nav from '../Nav/Nav';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <Nav />
         Home
       </div>
     )
